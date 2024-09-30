1510b298d4cc082a5cc3bd55817ec343f380339f
itsmeneha007 <nehabaghel11092003@gmail.com>
2024-09-30 21:36:06 +0530

    problem-3

---
+++ question-8/problem-3/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useToggleItems } from './useToggleItems';
+
+const App = () => {
+  
+  const [currentItem, toggleItem] = useToggleItems(["A", "B", "C"], 1);
+
+  return (
+    <div>
+      <h1>Current Item: {currentItem}</h1>
+      <button onClick={toggleItem}>Toggle Item</button>
+    </div>
+  );
+};
+
+export default App;
